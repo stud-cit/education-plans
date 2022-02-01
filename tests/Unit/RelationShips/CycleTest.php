d8268b855a2a52a559c0708b32a4523985fecc65
Ivan Zakharchenko <ipzakharchenko@gmail.com>
2022-02-01 19:36:30 +0200

    add (Cycle) migrate, seed, factory, model, test

---
+++ tests/Unit/RelationShips/CycleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\RelationShips;
+
+use PHPUnit\Framework\TestCase;
+
+class CycleTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
