e8639db555e376262fd90f26f47f7220bd1dc34c
Ivan Zakharchenko <ipzakharchenko@gmail.com>
2022-02-08 11:55:34 +0200

    add (translite files) uk launguge

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Created' => 'Created',
+    'Updated' => 'Updated',
+    'Up_to_date' => 'Up to date',
+    'Deleted' => 'Deleted',
+    'Page_not_found' => 'Page not found'
+];
---
+++ resources/lang/uk/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'Вказані облікові дані не збігаються з нашими записами.',
+    'password' => 'Наданий невірний пароль.',
+    'throttle' => 'Забагато спроб входу. Будь ласка, спробуйте ще раз, через :seconds секунд.',
+    'logout' => 'Ви покинули інформаційну систему.'
+
+];
