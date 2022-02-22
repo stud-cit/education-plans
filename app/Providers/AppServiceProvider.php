572e7f7fd4e9a03eb41310b18a8b68344eb32f84
Ivan Zakharchenko <ipzakharchenko@gmail.com>
2022-02-22 17:01:59 +0200

    add (paginateCollection) to app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (!Collection::hasMacro('paginateCollection')) {
+
+            Collection::macro('paginateCollection',
+                function ($perPage = 15, $page = null, $options = []) {
+                    $perPage = $perPage ?? 15; // if null given
+                    $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+                    return (new LengthAwarePaginator(
+                        $this->forPage($page, $perPage), $this->count(), $perPage, $page, $options))
+                        ->withPath('');
+                });
+        }
+
     }
 }
