d5211355630f6bc1c3d29b23d9b792683f9e398c
Maksim996 <12usa34airbornE@gmail.com>
2022-04-26 18:15:09 +0300

    Create ( FormOrganizationController.php ) add method index

--- app/Http/Controllers/FormOrganizationController.php
+++ app/Http/Controllers/FormOrganizationController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Resources\FormOrganizationResource;
+use App\Models\FormOrganization;
 
 class FormOrganizationController extends Controller
 {
-    //
+    public function index()
+    {
+        return FormOrganizationResource::collection(FormOrganization::select('id','title')->get());
+    }
 }
