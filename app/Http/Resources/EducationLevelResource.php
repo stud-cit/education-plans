a81691cad3f612b082e84c2df28080a8deb7b78b
Maksim996 <12usa34airbornE@gmail.com>
2022-04-26 18:18:21 +0300

    Update (EducationLevelResource.php) add id

--- app/Http/Resources/EducationLevelResource.php
+++ app/Http/Resources/EducationLevelResource.php
@@ -15,6 +15,7 @@ class EducationLevelResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'title' => $this->title
         ];
     }
