9fdc4db02e9120a1a892f63c02f4af9d9b196302
Maksim996 <12usa34airbornE@gmail.com>
2022-04-28 23:42:35 +0300

    Update (ExternalServices/ASU.php) add getWorkers, getAllWorkers, getFullNameWorker, searchFacultyByDepartmentId

--- app/ExternalServices/ASU.php
+++ app/ExternalServices/ASU.php
@@ -20,6 +20,11 @@ class ASU
     private const REJECTED_UNITS = [1571, 1150, 382];
     private const REJECTED_DIVISIONS = [339, 380];
     private const NOT_FOUND = 'Ідентифікатор не знайдено.';
+    private const WORKERS_TYPES = [
+        'EMPLOYEE' => 2,
+        'SUPERVISORS' => 4,
+        'MANAGERS' => 8,
+    ];
 
     function __construct() {
         $this->asu_key = config('app.asu_key');
@@ -67,7 +72,7 @@ class ASU
         return collect($results);
     }
 
-    public function getFaculty(): Collection
+    public function getFaculties(): Collection
     {
         $filtered = $this->getDepartments()->filter(function ($value) {
             return $value['unit_type'] == self::ID_FACULTY;
@@ -82,7 +87,7 @@ class ASU
      */
     public function getFacultyName(int $id): string
     {
-        $faculties = $this->getFaculty();
+        $faculties = $this->getFaculties();
 
         return $this->getName($faculties, $id);
     }
@@ -93,7 +98,7 @@ class ASU
      */
     public function getShortFacultyName(int $id): string
     {
-        $faculties = $this->getFaculty();
+        $faculties = $this->getFaculties();
 
         return $this->getShortName($faculties, $id);
     }
@@ -174,6 +179,74 @@ class ASU
         ];
 
         return  $this->getAsuData($this->url('getDivisions'), [], 'departments', $keys);
+    }
+
+    /**
+     * @param string $type
+     * @param bool $full
+     * @return Collection
+     */
+
+    public function getWorkers(string $type = 'EMPLOYEE', Bool $full = true): Collection
+    {
+        $url = $this->url('getContingents');
+        $queryParams = ['categ2' => self::WORKERS_TYPES[$type], 'mode' => $full];
+        $keys = [
+            "ID_FIO" => "asu_id",
+            "F_FIO" => "last_name",
+            "I_FIO" => "first_name",
+            "O_FIO" => "patronymic",
+            "KOD_DIV" => "department_id",
+            "NAME_DIV" => "department",
+        ];
+
+        return $this->getAsuData($url, $queryParams, $type, $keys);
+    }
+
+    /**
+     * @return Collection
+     */
+
+    public function getAllWorkers(): Collection
+    {
+        return $this->getWorkers('EMPLOYEE')
+            ->merge($this->getWorkers('SUPERVISORS'))
+            ->merge($this->getWorkers('MANAGERS'))->unique();
+    }
+
+    /**
+     * @param $asu_id
+     * @return String
+     */
+
+    public function getFullNameWorker($asu_id): String
+    {
+        $workers = $this->getAllWorkers();
+
+        $worker = $workers->first(function ($item) use ($asu_id) {
+            return $item['asu_id'] === $asu_id;
+        });
+
+        if (empty($worker)) {
+            return self::NOT_FOUND;
+        }
+
+        return $worker['last_name'] .' '. $worker['first_name'] .' '. $worker['patronymic'];
+    }
+
+    public function searchFacultyByDepartmentId($department_id)
+    {
+        $divisions = $this->getDepartments();
+
+        foreach ($divisions as $division)  {
+            if ($division['id'] == $department_id) {
+                if ($division['unit_type'] == self::ID_FACULTY || $division['unit_type'] == self::ID_INSTITUTE) {
+                    return $division;
+                } else {
+                    return $this->searchFacultyByDepartmentId($division['faculty_id']);
+                }
+            }
+        }
     }
 
     private function setQueryParams(Array $params): array
