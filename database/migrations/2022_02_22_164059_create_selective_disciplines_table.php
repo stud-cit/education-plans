375dc42ca707f44309661e9de5b03fff04b36187
Maksim996 <12usa34airbornE@gmail.com>
2022-05-04 13:49:03 +0300

    Update (migrations) selective_disciplines add timestamp

--- database/migrations/2022_02_22_164059_create_selective_disciplines_table.php
+++ database/migrations/2022_02_22_164059_create_selective_disciplines_table.php
@@ -16,6 +16,7 @@ class CreateSelectiveDisciplinesTable extends Migration
         Schema::create('selective_disciplines', function (Blueprint $table) {
             $table->id();
             $table->string('title', 150);
+            $table->timestamps();
         });
     }
 
