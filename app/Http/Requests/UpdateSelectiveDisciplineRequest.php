f8d73f4ffdedee12eaf774c2ec8ffb91e73ce37e
Maksim996 <12usa34airbornE@gmail.com>
2022-05-04 13:53:37 +0300

    Create (Requests) StoreSelectiveDisciplineRequest.php UpdateSelectiveDisciplineRequest.php

---
+++ app/Http/Requests/UpdateSelectiveDisciplineRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateSelectiveDisciplineRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => [
+                    'required',
+                    'string',
+                    'max:150',
+                    Rule::unique('selective_disciplines', 'title')->ignore($this->selective_discipline->id),
+                ]
+        ];
+    }
+}
