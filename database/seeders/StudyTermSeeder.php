e0d65f8968e0757423480dfea4d03e24375232a7
Maksim996 <12usa34airbornE@gmail.com>
2022-05-05 20:50:56 +0300

    Update (StudyTermSeeder.php) add new item

--- database/seeders/StudyTermSeeder.php
+++ database/seeders/StudyTermSeeder.php
@@ -15,12 +15,23 @@ class StudyTermSeeder extends Seeder
     public function run()
     {
         DB::table('study_terms')->Insert([
-            'title' => '3 роки 10 місяців',
-            'year' => 3,
-            'month' => 10,
-            'course' => 4,
-            'module' => 8,
-            'number_semesters' => 4,
-        ]);
+            [
+                'title' => '3 роки 10 місяців',
+                'year' => 3,
+                'month' => 10,
+                'course' => 4,
+                'module' => 8,
+                'number_semesters' => 8,
+            ],
+            [
+                'title' => '1 роки 10 місяців',
+                'year' => 1,
+                'month' => 10,
+                'course' => 2,
+                'module' => 8,
+                'number_semesters' => 4,
+            ]
+            ]
+        );
     }
 }
