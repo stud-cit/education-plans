fa3306a3fae53cd4e50f83aaa5b2da031ef71949
Ivan Zakharchenko <ipzakharchenko@gmail.com>
2022-06-09 16:48:06 +0300

    add (PositionSeeder) some data

--- database/seeders/PositionSeeder.php
+++ database/seeders/PositionSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PositionSeeder extends Seeder
 {
@@ -13,6 +14,13 @@ class PositionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('positions')->insert([
+            ['position' => 'Директор навчально-наукового інституту бізнесу, економіки та менеджменту'],
+            ['position' => 'Завідувач кафедри фінансів, банківської справи та страхування'],
+            ['position' => 'Керівник робочої проектної групи освітньої програми Фінанси та облік в підприємництві'],
+            ['position' => 'Керівник робочої проектної групи освітньої програми Державні та місцеві фінанси'],
+            ['position' => 'Керівник робочої проектної групи освітньої програми Банківська справа'],
+            ['position' => 'Проректор з науково-педагогічної діяльності'],
+        ]);
     }
 }
