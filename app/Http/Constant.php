f24386e2da7e94dbd83de700f88fa653c5c74806
Maksim996 <12usa34airbornE@gmail.com>
2022-06-16 13:57:09 +0300

    Update ( Constant.php ) add FORM_CONTROL

--- app/Http/Constant.php
+++ app/Http/Constant.php
@@ -5,4 +5,11 @@ namespace App\Http;
 class Constant
 {
     const PAGINATE = 15;
+
+    const FORM_CONTROL = [
+        'EXAM' => 1,
+        'DIFFERENTIAL_TEST' => 2,
+        'TEST' => 3,
+        'NO_CERTIFICATIONS' => 10
+    ];
 }
