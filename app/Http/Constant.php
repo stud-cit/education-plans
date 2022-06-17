2c519c6c1aaf77cee48be8deb6d04281db298262
Maksim996 <12usa34airbornE@gmail.com>
2022-06-17 11:52:25 +0300

    Update ( Constant.php ) add NUMBER_HOURS_IN_CREDIT, INDIVIDUAL_TASKS FORM_CONTROL->PROTECTION

--- app/Http/Constant.php
+++ app/Http/Constant.php
@@ -10,6 +10,15 @@ class Constant
         'EXAM' => 1,
         'DIFFERENTIAL_TEST' => 2,
         'TEST' => 3,
+        'PROTECTION' => 8,
         'NO_CERTIFICATIONS' => 10
+    ];
+
+    const NUMBER_HOURS_IN_CREDIT = 30;
+
+    const INDIVIDUAL_TASKS = [
+        'CONTROL_WORK' => 1,
+        'COURSE_WORK' => 2,
+        'NO_TASK' => 3,
     ];
 }
