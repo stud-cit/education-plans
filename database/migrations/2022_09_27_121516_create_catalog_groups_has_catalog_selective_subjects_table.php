3b8305b5544b8597954bfa5f92625f68d53480ee
Maksim996 <12usa34airbornE@gmail.com>
2022-09-27 12:37:21 +0300

    Create create_catalog_groups_has_catalog_selective_subjects_table.php

---
+++ database/migrations/2022_09_27_121516_create_catalog_groups_has_catalog_selective_subjects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCatalogGroupsHasCatalogSelectiveSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catalog_groups_has_catalog_selective_subjects', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('subject_id')->constrained('catalog_selective_subjects');
+            $table->foreignId('group_id')->constrained('catalog_groups');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catalog_groups_has_catalog_selective_subjects');
+    }
+}
