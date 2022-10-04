ece84f829171adf9104ede036de64f61a3628829
Ivan Zakharchenko <ipzakharchenko@gmail.com>
2022-10-04 17:21:07 +0300

    add (SubjectLanguageResouce) response ids

--- app/Http/Resources/SubjectLanguageResource.php
+++ app/Http/Resources/SubjectLanguageResource.php
@@ -15,6 +15,7 @@ class SubjectLanguageResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'title' => $this->title
         ];
     }
