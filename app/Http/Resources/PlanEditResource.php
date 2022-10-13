cf9c3a3d9115adb728eea8d25e315934bef13704
misha1997 <mishaotroshenko2013@gmail.com>
2022-10-13 13:09:18 +0300

    crutch

--- app/Http/Resources/PlanEditResource.php
+++ app/Http/Resources/PlanEditResource.php
@@ -128,7 +128,7 @@ class PlanEditResource extends JsonResource
         $querySubject->with('cycle')->whereHas('cycle', function ($queryCycle) use ($planId) {
           $queryCycle->where('plan_id', $planId);
         });
-      })->where($work)->where('semester', $semester)->count();
+      })->where($work)->where('semester', $semester)->where('hour', '!=', 0)->count();
       return $count;
     }
 
