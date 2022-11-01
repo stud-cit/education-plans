bd188d1b52d3ae79fd60f76e8f9c177351773193
Maksim996 <12usa34airbornE@gmail.com>
2022-11-01 10:49:32 +0200

    Fix (SubjectHelper)

--- database/seeders/CatalogHelperTypesSeeder.php
+++ database/seeders/CatalogHelperTypesSeeder.php
@@ -16,22 +16,21 @@ class CatalogHelperTypesSeeder extends Seeder
     {
         DB::table('catalog_helper_types')->insert([
             [
-                'title' => 'Компетентності (загальні та/або фахові,  на розвиток яких спрямована дисципліна',
-                'key' => 'catalog'
+                'title' => 'Загальна компетентність, на формування або розвиток якої спрямована дисципліна',
+                'key' => 'general_competence'
             ],
             [
                 'title' => 'Результати навчання за навчальною дисципліною',
-                'key' => 'catalog'
+                'key' => 'learning_outcomes'
             ],
             [
                 'title' => 'Види навчальних занять та методи викладання, що пропонуються',
-                'key' => 'catalog'
+                'key' => 'types_educational_activities'
             ],
             [
                 'title' => 'Вхідні вимоги до здобувачів, які хочуть обрати дисципліну/вимоги до матеріально-технічного забезпечення',
-                'key' => 'catalog'
+                'key' => 'entry_requirements_applicants'
             ],
-
         ]);
     }
 }
