fa0d411f3f191816bd4001b9d8538d7337cdf769
Ivan Zakharchenko <ipzakharchenko.gmail.com>
2022-11-08 19:27:28 +0200

    fix store rule for unique asu_id & catalog StoreCatalogSelectiveSubjectRequest

--- app/Http/Requests/StoreCatalogSelectiveSubjectRequest.php
+++ app/Http/Requests/StoreCatalogSelectiveSubjectRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreCatalogSelectiveSubjectRequest extends FormRequest
@@ -26,7 +27,14 @@ class StoreCatalogSelectiveSubjectRequest extends FormRequest
         return [
             'catalog_subject_id' => 'required|exists:App\Models\CatalogSubject,id',
             'catalog_education_level_id' => 'required|exists:App\Models\CatalogEducationLevel,id',
-            'asu_id' => 'required|max:255|unique:catalog_selective_subjects,asu_id,catalog_subject_id',
+            'asu_id' => [
+                'required',
+                'numeric',
+                Rule::unique('catalog_selective_subjects')->where(function ($query) {
+                    return $query->where('asu_id', $this->asu_id)
+                        ->where('catalog_subject_id', $this->catalog_subject_id);
+                })
+            ],
             'title' => 'required|string|max:255', // ?
             'title_en' => 'nullable|string|max:255', // ?
             'language' => 'required', // TODO: how validate?
