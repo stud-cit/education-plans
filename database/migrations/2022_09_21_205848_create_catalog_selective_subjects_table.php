2b8bb50ea298cedf1bc1fbdbd02d99ea3a946f10
Ivan Zakharchenko <ipzakharchenko.gmail.com>
2022-11-08 19:28:56 +0200

    ch asu_id string to integer in migration catalog_selective_subjects

--- database/migrations/2022_09_21_205848_create_catalog_selective_subjects_table.php
+++ database/migrations/2022_09_21_205848_create_catalog_selective_subjects_table.php
@@ -17,7 +17,7 @@ class CreateCatalogSelectiveSubjectsTable extends Migration
             $table->id();
             $table->foreignId('catalog_subject_id')->constrained('catalog_subjects', 'id');
             $table->foreignId('user_id')->constrained();
-            $table->string('asu_id', 60)->nullable(true); // 1
+            $table->integer('asu_id')->nullable(true); // 1
             $table->text('title')->nullable(true); // 1
             $table->text('title_en')->nullable(true); // 1
             // language in separate table 2
