53426cd6d2b2b1447d473b63f03aa4e2fd14dec4
Ivan Zakharchenko <ipzakharchenko.gmail.com>
2022-11-14 17:16:20 +0200

    fix user pagination UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Helpers\Helpers;
 use Illuminate\Http\Request;
 use App\ExternalServices\Asu\Worker;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\IndexUserRequest;
 use App\Http\Requests\StoreUserRequest;
-use App\Http\Resources\WorkersResource;
 use App\ExternalServices\Asu\Department;
 use App\Http\Requests\UpdateUserRoleRequest;
 
@@ -23,8 +24,12 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(IndexUserRequest $request)
     {
+        $validated = $request->validated();
+
+        $perPage = Helpers::getPerPage('items_per_page', $validated);
+
         $user = Auth::user();
         $roleAdmin = $user->role_id === User::ADMIN;
         $roleInstitute = $user->role_id === User::FACULTY_INSTITUTE;
@@ -37,7 +42,7 @@ class UserController extends Controller
                 return $query->whereIn('role_id', [User::FACULTY_INSTITUTE, User::DEPARTMENT])
                     ->where('faculty_id', '=', $user->faculty_id);
             })
-            ->paginate();
+            ->paginate($perPage);
 
         return UserResource::collection($users);
     }
