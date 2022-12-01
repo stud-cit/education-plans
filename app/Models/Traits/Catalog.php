00835f13db3f16ec5cf7b1df541a33c956ec69aa
Ivan Zakharchenko <ipzakharchenko.gmail.com>
2022-12-01 14:58:24 +0200

    add trait CatalogSubject

---
+++ app/Models/Traits/Catalog.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models\Traits;
+
+use App\Models\CatalogEducationLevel;
+use App\Helpers\Filters\FilterBuilder;
+use App\Models\CatalogSelectiveSubject;
+
+trait Catalog
+{
+    public function educationLevel()
+    {
+        return $this->belongsTo(CatalogEducationLevel::class, 'catalog_education_level_id');
+    }
+
+    public function subjects()
+    {
+        return $this->hasMany(CatalogSelectiveSubject::class, 'catalog_subject_id');
+    }
+
+    public function scopeFilterBy($query, $filters)
+    {
+        $namespace = 'App\Helpers\Filters\CatalogSubjectFilters';
+        $filter = new FilterBuilder($query, $filters, $namespace);
+
+        return $filter->apply();
+    }
+}
