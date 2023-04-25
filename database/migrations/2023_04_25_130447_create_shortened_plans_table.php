fe743fb32259e624ecdaf2dbc3bb13e595378936
Ivan Zakharchenko <ipzakharchenko.gmail.com>
2023-04-25 13:14:56 +0300

    add shortened_plans table

---
+++ database/migrations/2023_04_25_130447_create_shortened_plans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShortenedPlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shortened_plans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('plan_id')->constrained()->onDelete('cascade');
+            $table->foreignId('parent_id')->constrained('shortened_plans', 'plan_id')->onDelete('cascade');
+            $table->integer('shortened_by_year');
+            $table->unique(['plan_id', 'parent_id', 'shortened_by_year'], 'unique_shortened_plan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shortened_plans');
+    }
+}
