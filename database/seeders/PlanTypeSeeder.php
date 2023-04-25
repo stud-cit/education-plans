06d11ae1d6bd93b939a2c98394aa398bcb1317ff
Maksim996 <12usa34airbornE@gmail.com>
2023-04-25 13:53:14 +0300

    Fix name type seeder

--- database/seeders/PlanTypeSeeder.php
+++ database/seeders/PlanTypeSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class PlanTypeSeed extends Seeder
+class PlanTypeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
