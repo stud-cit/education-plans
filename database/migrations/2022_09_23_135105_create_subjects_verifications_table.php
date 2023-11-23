77edb4f2255eabe066c13653888f6e6a95afe783
Ivan Zakharchenko <ipzakharchenko@gmail.com>
2023-11-23 15:47:40 +0200

    fix(subjects_verifications): reference to table

--- database/migrations/2022_09_23_135105_create_subjects_verifications_table.php
+++ database/migrations/2022_09_23_135105_create_subjects_verifications_table.php
@@ -17,7 +17,7 @@ class CreateSubjectsVerificationsTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->foreignId('verification_status_id')->constrained();
-            $table->foreignId('subject_id')->constrained();
+            $table->foreignId('subject_id')->constrained('catalog_selective_subjects');
             $table->boolean('status');
             $table->text('comment')->nullable(true);
             $table->timestamps();
