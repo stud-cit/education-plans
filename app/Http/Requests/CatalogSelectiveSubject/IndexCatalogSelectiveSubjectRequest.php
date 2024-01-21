5905edc8cf4a17c60c09b280fff565850801d5eb
Maksim996 <12usa34airbornE@gmail.com>
2024-01-21 18:26:51 +0200

    Update (IndexCatalogSelectiveSubjectRequest.php) add required

--- app/Http/Requests/CatalogSelectiveSubject/IndexCatalogSelectiveSubjectRequest.php
+++ app/Http/Requests/CatalogSelectiveSubject/IndexCatalogSelectiveSubjectRequest.php
@@ -24,7 +24,7 @@ class IndexCatalogSelectiveSubjectRequest extends FormRequest
     public function rules()
     {
         return [
-            'year' => 'integer',
+            'year' => 'required|integer',
             'group' => 'integer',
             'faculty' => 'integer',
             'department' => 'integer',
