94b0caf6406a1d837a5ff8c5a585086dafd915b6
Ivan Zakharchenko <ipzakharchenko@gmail.com>
2026-06-02 20:25:18 +0300

    fix vue app variables

--- vue.config.js
+++ vue.config.js
@@ -1,52 +1,56 @@
 const path = require('path');
 
 module.exports = {
-  pages: {
-    index: {
-      entry: 'resources/js/main.js',
-      template: 'resources/views/spa-template.html',
-    }
-  },
-  outputDir: 'dist',
-  filenameHashing: false,
-  publicPath: '/',
-  transpileDependencies: [
-    'vuetify'
-  ],
-  lintOnSave: false,
-
-  chainWebpack: config => {
-    config.performance
-      .maxEntrypointSize(10000000)
-      .maxAssetSize(10000000)
-    // Don't copy files from public/ directory (Laravel's web root)
-    config.plugins.delete('copy');
-
-    // Inject backend env vars into frontend as VUE_APP_* (avoids duplicating in .env)
-    config.plugin('define').tap(args => {
-      const env = args[0]['process.env'];
-      env.VUE_APP_CABINET_APP_URL = JSON.stringify(process.env.CABINET_APP_URL);
-      env.VUE_APP_CABINET_APP_TOKEN = JSON.stringify(process.env.CABINET_APP_TOKEN);
-      env.VUE_APP_CABINET_APP_SERVICE = JSON.stringify(process.env.CABINET_APP_SERVICE || 'index/service/');
-      return args;
-    });
-  },
-
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'resources/js/'),
-        '@c': path.resolve(__dirname, 'resources/js/components/'),
-      },
+    pages: {
+        index: {
+            entry: 'resources/js/main.js',
+            template: 'resources/views/spa-template.html',
+        }
     },
-  },
+    outputDir: 'dist',
+    filenameHashing: false,
+    publicPath: '/',
+    transpileDependencies: [
+        'vuetify'
+    ],
+    lintOnSave: false,
 
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:80',
-        changeOrigin: true,
-      },
+    chainWebpack: config => {
+        config.performance
+            .maxEntrypointSize(10000000)
+            .maxAssetSize(10000000)
+        // Don't copy files from public/ directory (Laravel's web root)
+        config.plugins.delete('copy');
+
+        // Inject backend env vars into frontend as VUE_APP_* (avoids duplicating in .env)
+        config.plugin('define').tap(args => {
+            const env = args[0]['process.env'];
+            env.VUE_APP_CABINET_APP_URL = JSON.stringify(process.env.CABINET_APP_URL);
+            env.VUE_APP_CABINET_APP_TOKEN = JSON.stringify(process.env.CABINET_APP_TOKEN);
+            env.VUE_APP_CABINET_APP_SERVICE = JSON.stringify(process.env.CABINET_APP_SERVICE || 'index/service/');
+            env.VUE_APP_CABINET_APP_SERVICE = JSON.stringify(process.env.CABINET_APP_SERVICE || 'index/service/');
+            env.VUE_APP_NAME_APP = JSON.stringify(process.env.VUE_APP_NAME_APP);
+            env.VUE_APP_DEBUG = JSON.stringify(process.env.VUE_APP_DEBUG);
+            env.VUE_APP_BY_CREATED_PDF = JSON.stringify(process.env.VUE_APP_BY_CREATED_PDF);
+            return args;
+        });
     },
-  },
+
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'resources/js/'),
+                '@c': path.resolve(__dirname, 'resources/js/components/'),
+            },
+        },
+    },
+
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:80',
+                changeOrigin: true,
+            },
+        },
+    },
 };
