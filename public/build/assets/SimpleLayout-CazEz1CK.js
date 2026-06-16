9d4cd1760bf31bb905541b085f8097063ab5812d
misha1997 <mishaotroshenko2013@gmail.com>
2026-06-16 17:37:16 +0300

    remote build folder

--- public/build/assets/SimpleLayout-CazEz1CK.js
+++
@@ -1,1 +0,0 @@
-import{n as r}from"./main-D6pi945I.js";const t={};var o=function(){var e=this,n=e._self._c;return n("router-view")},_=[],s=r(t,o,_,!1,null,null);const l=s.exports;export{l as default};
